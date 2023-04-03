efd8d2239ad9d072a489f4427808db4ccc5ba938
emmanuel-va1d <vald.emmanuel04@gmail.com>
2023-04-03 14:10:40 -0700

    Created 3 variables for the calculator: the first number, the operator, and the second number. I then created a function, which takes those three variables as arguements, and essentially passes one of the calculator functions as the operator argument

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
+
+let num1;
+let num2;
+let operator;
+
+const operate = (x, y, op) => op(x, y);
