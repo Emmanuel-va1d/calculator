163d6fa0b828c7896c13552dddb486404790402b
emmanuel-va1d <vald.emmanuel04@gmail.com>
2023-04-04 19:51:09 -0700

    Got most of the calculators events working, which is only outputing to the screen. Actual calculations are still not completed. Created functions for the all clear button and delete button. The delete button has checks to make sure the output is 0 if there are no longer values to be deleted. Also changed the classes for most of the buttons. The number buttons, instead, have a collective class in order to select them all. The main operators have a second class for outputting purposes. I will figure out whether or not I will need both classes after working with producing actual calculations.

--- script.js
+++ script.js
@@ -2,9 +2,51 @@ const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
-
-let num1;
-let num2;
-let operator;
-
 const operate = (x, y, op) => op(x, y);
+
+
+/* input */
+const number = document.querySelectorAll(".num");
+const operator = document.querySelectorAll(".operator");
+const equal = document.querySelector(".enter");
+
+/* action */
+const clear = document.querySelector(".clear");
+const del = document.querySelector(".delete");
+
+/* output */
+const output = document.querySelector("h1");
+const history = document.querySelector("h2");
+var s = ""
+
+
+/* event listeners */
+number.forEach(num => num.addEventListener("click", () => {
+    s += num.textContent;
+    output.textContent = s;
+}));
+
+operator.forEach(op => op.addEventListener("click", () => {
+    s += ` ${op.textContent} `;
+    output.textContent = s;
+}));
+
+equal.addEventListener("click", () => {
+    history.textContent = s;
+})
+
+clear.addEventListener("click", () => {
+    output.textContent = "0";
+    history.textContent = "-";
+    s = "";
+})
+
+del.addEventListener("click", () => {
+    if (output.textContent !== "0" && s.length !== 1) {
+        s = s.slice(0, -1)
+        output.textContent = s;
+    } else if (s.length == 1) {
+        s = ""
+        output.textContent = "0";
+    }
+});
